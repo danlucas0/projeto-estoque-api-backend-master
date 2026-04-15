a1e4845e24bb6473c53270791a7333143649d9ba
danlucas0 <dinholucas1002@gmail.com>
2026-04-15 18:49:37 -0300

    att movimetacoes service

--- src/movimentacoes/movimentacoes.service.ts
+++ src/movimentacoes/movimentacoes.service.ts
@@ -1,9 +1,11 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Movimentacao } from './movimentacao.entity';
-import { Produto } from 'src/produtos/produto.entity';
-import { CreateMovimentacaoDto, TipoMovimentacao } from './dto/CreateMovimentacaoDto';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Movimentacao } from './movimentacao.entity'
+import { Produto } from 'src/produtos/produto.entity'
+import { CreateMovimentacaoDto, TipoMovimentacao } from './dto/CreateMovimentacaoDto'
+
+const LIMITE_ESTOQUE = 999999
 
 @Injectable()
 export class MovimentacaoService {
@@ -16,32 +18,56 @@ export class MovimentacaoService {
   ) {}
 
   async create(dto: CreateMovimentacaoDto): Promise<Movimentacao> {
-    const { produtoId, tipo, quantidade } = dto;
+    const { produtoId, tipo, quantidade } = dto
 
-    const produto = await this.produtoRepository.findOne({ where: { id: produtoId } });
+    const produto = await this.produtoRepository.findOne({
+      where: { id: produtoId },
+    })
 
     if (!produto) {
-      throw new NotFoundException(`Produto com id ${produtoId} não encontrado`);
+      throw new NotFoundException(`Produto com id ${produtoId} não encontrado`)
+    }
+
+    if (!Number.isInteger(quantidade) || quantidade <= 0) {
+      throw new BadRequestException(
+        'A quantidade deve ser um número inteiro maior que zero.',
+      )
     }
 
     if (tipo === TipoMovimentacao.SAIDA) {
       if (produto.estoque < quantidade) {
         throw new BadRequestException(
           `Estoque insuficiente. Disponível: ${produto.estoque}, solicitado: ${quantidade}`,
-        );
+        )
       }
-      produto.estoque -= quantidade;
+
+      produto.estoque -= quantidade
     } else {
-      produto.estoque += quantidade;
+      const novoEstoque = produto.estoque + quantidade
+
+      if (novoEstoque > LIMITE_ESTOQUE) {
+        throw new BadRequestException(
+          `O estoque não pode ultrapassar ${LIMITE_ESTOQUE}. Estoque atual: ${produto.estoque}.`,
+        )
+      }
+
+      produto.estoque = novoEstoque
     }
 
-    await this.produtoRepository.save(produto);
+    await this.produtoRepository.save(produto)
 
-    const movimentacao = this.movimentacaoRepository.create({ produto, tipo, quantidade });
-    return await this.movimentacaoRepository.save(movimentacao);
+    const movimentacao = this.movimentacaoRepository.create({
+      produto,
+      tipo,
+      quantidade,
+    })
+
+    return await this.movimentacaoRepository.save(movimentacao)
   }
 
   async findAll(): Promise<Movimentacao[]> {
-    return this.movimentacaoRepository.find({ order: { data: 'DESC' } });
+    return this.movimentacaoRepository.find({
+      order: { data: 'DESC' },
+    })
   }
 }
